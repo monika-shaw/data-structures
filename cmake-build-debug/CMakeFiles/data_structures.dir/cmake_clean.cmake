e24e23c67b90c9c2e09c0f861fb18e40e134eb3c
shawmonika616ms <shawmonika616ms@gmail.com>
2023-01-26 19:21:23 +0530

    implemented linear search

---
+++ cmake-build-debug/CMakeFiles/data_structures.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structures.dir/main.cpp.obj"
+  "CMakeFiles/data_structures.dir/main.cpp.obj.d"
+  "data_structures.exe"
+  "data_structures.exe.manifest"
+  "data_structures.pdb"
+  "libdata_structures.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
